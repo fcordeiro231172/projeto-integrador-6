088fff13400d0b78f03f2e3238c17b0e72f839af
fcordeiro231172 <flaviocordeiro@globo.com>
2018-08-21 11:45:11 -0300

    Mudei o index.html para .php / criei o arquivo header.php / fiz o include do header.php no index.php dentro da div header.

---
+++ header.php
@@ -0,0 +1,94 @@
+
+<nav class="navbar navbar-fixed-top navbar-default" style="height: 70px; background-color: white;">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#" style="padding-top:10px;">
+        <img src="imagens/logo_sense_2.png" alt="logo" width=120px
+        >
+      </a>
+    </div>
+
+    <!-- MENU PRINCIPAL-->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav" style="margin-left: 100px">
+
+        <!-- MAQUIAGEM -->
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+          aria-haspopup="true" aria-expanded="false" style="margin-top:25px; padding: 12px 24px;">
+          Maquiagem <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Rosto</a></li>
+            <li><a href="#">Olhos</a></li>
+            <li><a href="#">Boca</a></li>
+            <li><a href="#">Paletas</a></li>
+            <li><a href="#">Kits</a></li>
+            <li><a href="#">Acessórios</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"><img src="imagens/maquiagem2_menu.jpg" alt="maquiagem" width= 150px>Conheça nossa linha completa de maquiagens!</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"><strong>NEW! &nbsp &nbsp ASSINE "Sense Sempre Comigo". CLIQUE AQUI!</strong></a></li>
+          </ul>
+        </li>
+        <!-- PERFUMARIA -->
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+          aria-haspopup="true" aria-expanded="false" style="margin-top:25px; padding: 12px 24px;">
+          Perfumaria <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Feminimo</a></li>
+            <li><a href="#">Masculino</a></li>
+            <li><a href="#">Para presente</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"><img src="imagens/perfumaria_menu.jpg" alt="maquiagem" width= 150px>Conheça nossa linha completa de perfumes!</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"><strong>NEW! &nbsp &nbsp ASSINE "Sense Sempre Comigo". CLIQUE AQUI!</strong></a></li>
+          </ul>
+        </li>
+        <!-- COSMÉTICOS -->
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+          aria-haspopup="true" aria-expanded="false" style="margin-top:25px; padding: 12px 24px;">
+          Cosméticos <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Cuidado facial</a></li>
+            <li><a href="#">Cuidado corporal</a></li>
+            <li><a href="#">Cuidado capilar</a></li>
+            <li><a href="#">Kits</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"><img src="imagens/cosmetico_menu.jpg" alt="maquiagem" width= 150px>Conheça nossa linha completa de cosméticos!</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"><strong>NEW! &nbsp &nbsp ASSINE "Sense Sempre Comigo". CLIQUE AQUI!</strong></a></li>
+          </ul>
+        </li>
+      </ul>
+
+      <!-- BUSCA -->
+      <form class="navbar-form navbar-left" style="margin-left:120px; margin-top: 25px">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Digite aqui...">
+          <span class=input-group-btn>
+        <button type="submit" class="btn btn-default" style="color: gray">Buscar</button>
+        </span>
+      </div>
+      </form>
+
+      <!-- lOGIN e CART -->
+      <div class="compras">
+        <a href="login">
+          <p>Olá, <a href="#" class="navbar-link">Flávio Cordeiro</a></p>
+        </a>
+        <a href="carrinho.html">
+          <ul>
+            <li>Minhas Compras</li><li><img class="carrinho"src="imagens/sacola2.jpg" alt="carrinho"></li>
+          </ul>
+        </a>
+      </div>
+    </div><!-- /.navbar-collapse -->
+</nav>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Integrador-6</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="container-fluid">
+
+      <!-- HEADER -->
+      <div class="header">
+        <?php include ("header.php"); ?>
+      </div>
+
+
+      <!-- BANNER -->
+      <div class="row">
+      </div>
+
+      <!-- PRODUTOS EM DESTAQUE -->
+      <div class="row">
+      </div>
+
+      <!-- FOOTER -->
+      <div class="footer">
+        Footer será criado por Flávio
+      </div>
+
+  </div>
+
+<!-- link do JavaScript do jQUERY  -->
+<script
+src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+crossorigin="anonymous"></script>
+
+<!-- link do JavaScript do BOOTSTRAP  -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+crossorigin="anonymous"></script>
+
+</body>
+</html>
