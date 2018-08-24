6faefff197dbc6fb144ef3fe3301d85fe740c48f
fcordeiro231172 <flaviocordeiro@globo.com>
2018-08-24 18:19:43 -0300

    Acerto do responsivo da página login.php

--- login.php
+++ login.php
@@ -19,32 +19,36 @@
 
 <div class="total col-lg-12">
 
-        <div class="titulo col-lg-12">
+        <div class="titulo col-xs-12">
           <h2>Formulário de Login</h2>
         </div>
 
-        <div class= "foto col-lg-3">
+        <div class= "foto col-lg-3 col-md-4 col-sm-6">
           <img src="imagens/giselemaq1.jpg" alt="Gisele Bündchen" title="Gisele Bündchen">
         </div>
 
-        <form action="/action_page.php" class= "col-lg-4">
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Digite seu email" name="email">
-            </div>
-            <div class="form-group" >
-                <label for="pwd">Senha</label>
-                <input type="password" class="form-control" id="pwd" placeholder="Digite sua senha" name="pswd">
-            </div>
-            <div class="form-group form-check">
-                <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" name="remember"> Lembrar
-              </label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <div class="formulario col-lg-4 col-md-4 col-sm-6">
+            <form action="/action_page.php">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Digite seu email" name="email">
+                </div>
+                <div class="form-group" >
+                    <label for="pwd">Senha</label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Digite sua senha" name="pswd">
+                </div>
+                <div class="form-group form-check">
+                    <label class="form-check-label">
+                    <input class="form-check-input" type="checkbox" name="remember"> Lembrar
+                  </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+      </div>
+      <div class="branco col-lg-5 col-md-4 col-sm-0">
+      </div>
 
-        <div class="botredes col-lg-6">
+      <div class="botredes col-xs-6">
            <a href="#" class="fb btn">
              <i class="fa fa-facebook fa-fw"></i> Login with Facebook
               </a>
